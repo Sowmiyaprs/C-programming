9cffe18bba14044019d52ab02977b9875dde3676
Sowmiya Periyasamy <periyasamysowmiya339@gmail.com>
2024-09-20 21:54:31 +0530

    Update

---
+++ Biodata.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("PERSONAL INFORMATION\n");
+    printf("NAME          :SOWMIYA P\n");
+    printf("FATHER NAME   : P PERIYASAMY\n");
+    printf("MOTHER NAME   : P RENUKA\n");
+    printf("DATE OF BIRTH :18.04.2007\n");
+    printf("GENDER        :FEMALE\n");
+    printf("PHONE NUMBER  :9597293558\n");
+    printf("MAIL ID       :abc12@gmail.com\n");
+    printf("CITY:TIRUPPUR\n");
+
+    printf("\nSKILLS\n");
+    printf("PROGRAMMING LANGUAGES: Java HTML C programming\n");
+
+    printf("\nHOBBIES LIKE:\n");
+    printf("Classical Dancer\nReading Books\nWatching Movies\nTravelling\n");
+}
