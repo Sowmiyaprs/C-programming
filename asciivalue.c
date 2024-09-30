f83f0405e5177a1a3a4be9086e4c7731bf9607b0
Sowmiya Periyasamy <periyasamysowmiya339@gmail.com>
2024-09-30 15:00:32 +0530

    Create asciivalue.c

---
+++ asciivalue.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+  
+  char ch='A';
+  printf("Print the ASCII value of %c: %d ",ch,ch);
+                 
+               //here we are putting 2times ch ,because there is 2 formal specifier
+}
+
+
+
