9bc135ec4cc190a9d703079f0332c19201fe7755
Sowmiya Periyasamy <periyasamysowmiya339@gmail.com>
2024-09-30 15:50:24 +0530

    Create scanner.c

---
+++ scanner.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+  
+ int age;
+ int gender;
+ scanf("%d %c",&age,&gender);
+ 
+     //there is a space btw 2formal specifier because after type the age we will give 
+     //an enter hence enter is also a special char so we need to give space button
+   
+}
+
+
+
+
