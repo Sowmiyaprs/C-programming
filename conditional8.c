47113a859a2de82871ee643e24152cb171769a12
Sowmiya Periyasamy <periyasamysowmiya339@gmail.com>
2024-10-28 14:31:02 +0530

    Create conditional8.c

---
+++ conditional8.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+  int num;
+  scanf("%d",&num);
+  
+  if(num%2==0)
+  {
+      printf("IT IS EVEN NUMBER");
+  }
+  else
+  {
+      printf("IT IS ODD NUMBER");
+  }
+  
+}
+
